c85e4283ecb5bc78e26917b8d3f6125939498384
leyla1991 <atagleyla@mail.ru>
2024-10-05 21:30:07 +0300

    Тех.задание "Кинотеатр".

--- job4j_cinema/src/main/resources/db/scripts/004_dml_insert_films.sql
+++
@@ -1,8 +0,0 @@
-insert into films( name, description, "year", genre_id, minimal_age,
-    duration_in_minutes, file_id) values ('Джава и ее друзья', 'Базы данных встречают Джаву и Спринг', 2023, 3, 5, 180, 3);
-insert into films( name, description, "year", genre_id, minimal_age,
-    duration_in_minutes, file_id) values ('Джавист встретил Хелоу ворлд', 'Драматическая история', 2023, 3, 18, 180, 3);
-insert into films( name, description, "year", genre_id, minimal_age,
-    duration_in_minutes, file_id) values ('Джавист и его первая работа', 'Ужасы статиста', 2023, 4, 18, 180, 2);
-insert into films( name, description, "year", genre_id, minimal_age,
-    duration_in_minutes, file_id) values ('Джавист на пути Мидла', 'Ужасы статиста', 2023, 4, 18, 180, 2);
